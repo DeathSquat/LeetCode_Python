820517c77f139078e7c28e19f10b6f785d329266
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-01-09 22:59:17 +0530

    25. Reverse Nodes in k-Group.py

--- 25. Reverse Nodes in k-Group.py
+++ 25. Reverse Nodes in k-Group.py
@@ -6,7 +6,7 @@
 class Solution(object):
     def reverseKGroup(self, head, k):
         """
-        :type head: Optional[ListNode]
+        :type head:Optional[ListNode]
         :type k: int
         :rtype: Optional[ListNode]
         """
