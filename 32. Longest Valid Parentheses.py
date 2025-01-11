c19f0a53b016a9a2051baba35cf4aeb41dccb00c
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-01-11 10:21:24 +0530

    32. Longest Valid Parentheses.py

---
+++ 32. Longest Valid Parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_length = 0
+        stack = [-1]  # Initialize stack with a base index
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                # Push the index of '(' onto the stack
+                stack.append(i)
+            else:
+                # Pop the last index from the stack
+                stack.pop()
+                if not stack:
+                    # If the stack is empty, push the current index
+                    stack.append(i)
+                else:
+                    # Calculate the length of the current valid substring
+                    max_length = max(max_length, i - stack[-1])
+        
+        return max_length
