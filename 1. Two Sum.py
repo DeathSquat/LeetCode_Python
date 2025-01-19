8664cf51d33146daa8d4c584c84f8b0c3f50dcd5
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-01-19 19:41:00 +0530

    1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -6,7 +6,7 @@ class Solution(object):
             # Calculate the complement
             complement = target - num
         
-            # Check if the complement exists in the dictionary
+            # Check if the complement exists in dictionary
             if complement in num_map:
                 return [num_map[complement], i]
         
